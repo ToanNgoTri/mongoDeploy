487237692b5c5c583d3c3150cbefd3dba618dfa1
ToanNgoTri <ngotritoan33@gmail.com>
2024-03-01 10:08:35 +0700

    c

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ mongoose.set('strictQuery',false);
 // }
 
 mongoose
-  .connect((process.env.MONGODB_CONNECT_URI))
+  .connect((process.env.MONGO_URI))
   .then(() => console.log(`mongoDB Connected`))
   .catch( (err) => console.log(err))
 
